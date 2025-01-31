359c148624b516b2d435dc70c9f349f8bc3be04a
Márcio Machado Ribeiro <jamesfoxbr@hotmail.com>
2025-01-31 15:43:55 -0300

    ImGui interface starting to be placed for test. Still not sure what  works or not

--- Raylib Starfield/main.cpp
+++ Raylib Starfield/main.cpp
@@ -2,12 +2,20 @@
 #include "Globals.h"
 #include "Window.h"
 #include "Controls.h"
+#include "imgui.h"
 #include "rlImGui.h"	
 
 int main() {
     Window window;
     Controls control;
     Space space(window.camera);
+
+    rlImGuiSetup(true); 	// sets up ImGui with ether a dark or light default theme
+    // Our state
+    bool show_demo_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImVec4(0.0f, 0.0f, 0.0f, 0.0f);
+
 
     // Main game loop
     while (!WindowShouldClose()) 
@@ -16,7 +24,7 @@ int main() {
         control.Update();
 
         BeginDrawing(); 
-            ClearBackground(BLACK);
+            ClearBackground(Color{(unsigned char)(clear_color.x * 255), (unsigned char)(clear_color.y * 255), (unsigned char)(clear_color.z * 255), (unsigned char)(clear_color.w * 255)});
 
             // 3D stuff go inside this area
             BeginMode3D(window.camera); 
@@ -26,17 +34,43 @@ int main() {
 
             // end of the 3d drawing area
             EndMode3D();
-
-            space.Draw2D();
-
-            // 2D stuff fized to the screen space go in this area
+            // 2D stuff fixed to the screen space go in this area
+            
+                space.Draw2D();
                 DrawCoordinates(window.camera);
                 //targetCursor();
 			    DrawDebugText("FPS: " + std::to_string(GetFPS()), 10, 30, 20, RAYWHITE);
 			    DrawDebugText(std::to_string(space.GetNumberOfStarfields()) + " starfields", 10, 50, 20, RAYWHITE);
 			    DrawDebugText(std::to_string(space.GetNumberOfStars()) + " stars", 10, 70, 20, RAYWHITE);
 
+                rlImGuiBegin();			// starts the ImGui content mode. Make all ImGui calls after this
+                {
+                    
+                    static float f = 0.0f;
+                    static int counter = 0;
+
+                    ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+                    ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+                    ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+                    ImGui::Checkbox("Another Window", &show_another_window);
+
+                    ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+                    ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+                    if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                        counter++;
+                    ImGui::SameLine();
+                    ImGui::Text("counter = %d", counter);
+
+                    //ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+                    ImGui::End();
+                }
+                rlImGuiEnd();			// ends the ImGui content mode. Make all ImGui calls before this
+
         EndDrawing(); 
     }
+
+    rlImGuiShutdown();		// cleans up ImGui
     return 0;
 }
